a3f7db5572e3fb22e43630c7ac0b88e437157e78
Mary Alice Orr <m.a.orr17@gmail.com>
2018-06-23 19:20:27 -0500

    wip commit on styling

--- javascripts/domStrang.js
+++ javascripts/domStrang.js
@@ -13,21 +13,24 @@ const blogCards = (blogArray) => {
 const projectCards = (projectArray) => {
   let strang2 = '';
   projectArray.forEach((project) => {
-    strang2 += `<div class="project-cards">`;
-    strang2 +=   `<div id="${project.id}">`;
-    strang2 +=     `<h2>${project.title}</h2>`;
-    strang2 +=     `<img class="project-image" src="${project.imageURL}">`;
-    strang2 +=     `<p>${project.description}</p>`;
-    strang2 +=     `<a href="${project.githubURL}">${project.id}</a>`;
-    strang2 +=     `<div>`;
-    strang2 +=      `<h4>Technologies Used:</h4>`;
-
+    strang2 += `<div class="panel project-cards">`;
+    strang2 +=  `<div class="panel-heading">`;
+    strang2 +=    `<h2 class="panel-title text-center">${project.title}</h2>`;
+    strang2 +=  `</div>`;
+    strang2 +=  `<div class="panel-body">`;
+    strang2 +=    `<p class="project-card-descrip">${project.description}</p>`;
+    strang2 +=    `<img class="project-image" src="${project.imageURL}">`;
+    strang2 +=    `<a href="${project.githubURL}">${project.id}</a>`;
+    strang2 +=  `</div>`;
+    strang2 +=  `<div class="panel-footer">`;
+    strang2 +=    `<h4 class="text-center">Technologies Used</h4>`;
+    strang2 +=    `<div class="center">`;
     project.badges.forEach((badge) => {
-      strang2 +=      `<img src=${badge.badgeImg}/> `;
+      strang2 +=    `<img class="project-badges center" src="${badge.badgeImg}"/>`;
     });
-
-    strang2 +=     `</div>`;
+    strang2 +=    `</div>`;
     strang2 +=   `</div>`;
+    strang2 +=  `</div>`;
     strang2 += `</div>`;
   });
   writeToDom('#projects-holder', strang2);
